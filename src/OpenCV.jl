3de814e5a5f203cd2739aaaa631bbd307707724c
rakesh <rakeshksr@users.noreply.github.com>
2023-10-08 17:59:34 +0530

    Stream support for FileIO and show method

--- src/OpenCV.jl
+++ src/OpenCV.jl
@@ -1,6 +1,7 @@
 module OpenCV
 
 using OpenCV_jll
+using FileIO: File, DataFormat, Stream, stream
 
 include(joinpath(OpenCV_jll.artifact_dir, "OpenCV", "src", "OpenCV.jl"))
 
--- src/fileio.jl
+++ src/fileio.jl
@@ -1,24 +1,46 @@
-using FileIO: File, DataFormat, Stream, stream
-
 const _IMAGE_FORMATS = (:PNG, :JPEG)
-const _IMAGE_FILE_FORMATS = Union{(File{DataFormat{T}} for T in _IMAGE_FORMATS)...}
+const _IMAGE_DATA_FORMATS = Union{(DataFormat{f} for f in _IMAGE_FORMATS)...}
 
-function load(f::T) where {T<:_IMAGE_FILE_FORMATS}
+function _get_format_extension(fmt)
+    FileIO.info(fmt)[2]
+end
+
+## Load Images
+function load(f::File{T}) where {T<:_IMAGE_DATA_FORMATS}
     data = imread(f.filename)
     return data
 end
 
-function load(f::T, flags::Int) where {T<:_IMAGE_FILE_FORMATS}
+function load(f::File{T}, flags::Int) where {T<:_IMAGE_DATA_FORMATS}
     data = imread(f.filename, flags)
     return data
 end
 
-function save(f::T, image::InputArray) where {T<:_IMAGE_FILE_FORMATS}
+function load(s::Stream{T}, flags::Int=IMREAD_UNCHANGED) where {T<:_IMAGE_DATA_FORMATS}
+    data = read(stream(s))
+    img = imdecode(reshape(data, 1, 1, :), flags)
+    return img
+end
+
+## Save Images
+
+function save(f::File{T}, image::InputArray) where {T<:_IMAGE_DATA_FORMATS}
     imwrite(f.filename, image)
 end
 
-function save(f::T, image::InputArray, params::Array{Int32,1}) where {T<:_IMAGE_FILE_FORMATS}
+function save(f::File{T}, image::InputArray, params::Array{Int32,1}) where {T<:_IMAGE_DATA_FORMATS}
     imwrite(f.filename, image, params)
 end
 
-## To-do add streams
+function save(s::Stream{T}, image::InputArray) where {T<:_IMAGE_DATA_FORMATS}
+    ext = _get_format_extension(T)
+    enc_img = imencode(ext, image)
+    write(stream(s), enc_img)
+end
+
+function save(s::Stream{T}, image::InputArray, params::Vector{Int32}) where {T<:_IMAGE_DATA_FORMATS}
+    ext = _get_format_extension(T)
+    enc_img = imencode(ext, image, params)
+    write(stream(s), enc_img)
+end
+
